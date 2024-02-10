69382b231cbb0625391b2e522a896be334c9519e
specialtas <specialtas@aol.com>
2024-02-10 11:39:24 -0600

    commiting these recent changes to goal flow

--- src/main/java/org/example/controllers/GoalsController.java
+++ src/main/java/org/example/controllers/GoalsController.java
@@ -1,6 +1,8 @@
 package org.example.controllers;
 
+import org.example.data.GoalRepository;
 import org.example.models.Goals;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GoalsController {
+
+    @Autowired
+    private GoalRepository;
     @GetMapping("/goal-form")
     public String showGoalForm(Model model) {
         model.addAttribute("goal", new Goals());
