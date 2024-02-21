24b0ecfaa1bc71a103803b8a5a2c834f1e5a155c
specialtas <specialtas@aol.com>
2024-02-20 21:34:18 -0600

    adjusted some files to make it run

--- src/main/java/org/example/controllers/GoalsController.java
+++ src/main/java/org/example/controllers/GoalsController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,9 +24,16 @@ public class GoalsController {
         return "goal-form";
     }
     @PostMapping("/submit-goal")
-    public String submitGoalForm(@ModelAttribute Goals goal) {
+    public String submitGoalForm(@ModelAttribute Goals goal, @RequestParam(value = "completed", required = false) boolean completed, @RequestParam(value = "approved", required = false) boolean approved) {
+        goal.setCompleted(completed);
+        if (completed) {
+            goal.setCompletedDate(LocalDate.now());
+        }
+        goal.setApproved(approved);
+        if (approved) {
+            goal.setApprovedDate(LocalDate.now());
+        }
         goalRepository.save(goal);
-        // Handle form submission here will eventually connect to SQL database
         return "redirect:/goals";
     }
     @GetMapping("/goals")
