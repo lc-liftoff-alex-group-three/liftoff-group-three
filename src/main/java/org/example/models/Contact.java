6418395e436446fe3a8e7d3f34f06f233ccb7533
Christi-Lee <christileecurrier@gmail.com>
2024-02-20 20:36:07 -0600

    created model form etc

--- src/main/java/org/example/models/Contact.java
+++ src/main/java/org/example/models/Contact.java
@@ -1,4 +1,63 @@
 package org.example.models;
 
+
+
+
 public class Contact {
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String message;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "userName='" + userName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
