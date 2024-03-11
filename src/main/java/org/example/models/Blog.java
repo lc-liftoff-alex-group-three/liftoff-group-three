175624410c9288664b5aeba334274f7abc12310c
Christi-Lee <christileecurrier@gmail.com>
2024-03-11 18:02:59 -0500

    D3 html and design

--- src/main/java/org/example/models/Blog.java
+++ src/main/java/org/example/models/Blog.java
@@ -1,24 +1,28 @@
 package org.example.models;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 public class Blog {
 
     private int id;
     private static int nextId;
-
+@NotBlank(message = "Title can not be blank.")
+@Size
     private String title;
+@NotBlank
     private String content;
     private LocalDate time;
+    @NotBlank
     private String status;
 
     public Blog(String title, String content, String status) {
         this.title = title;
         this.content = content;
         this.status = status;
+        this.time = LocalDate.now();
         this.id = nextId;
         nextId++;
-        this.time = LocalDate.now();
 
     }
 
