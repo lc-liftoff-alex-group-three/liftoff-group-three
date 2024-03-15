c0c77692963d44d63792a9c772a5fcaf261d4a7d
Christi-Lee <christileecurrier@gmail.com>
2024-03-14 19:15:06 -0500

    fixed render of new blog post display

--- src/main/java/org/example/controllers/BlogController.java
+++ src/main/java/org/example/controllers/BlogController.java
@@ -3,11 +3,12 @@ package org.example.controllers;
 
 import org.example.data.BlogData;
 import org.example.models.Blog;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+@Controller
 class BlogController {
 
     private BlogData blogData   = new BlogData();
@@ -26,9 +27,10 @@ class BlogController {
     }
 
     @PostMapping("/new-post")
-    public String handlePostForm(Model model,  @ModelAttribute Blog blog){
-       model.addAttribute("blog", String.valueOf(blog));
+    public String handlePostForm(@ModelAttribute Blog blog, Model model) {
         blogData.add(blog);
+        model.addAttribute("blog", blog);
         return "displayPost";
     }
+
 }
