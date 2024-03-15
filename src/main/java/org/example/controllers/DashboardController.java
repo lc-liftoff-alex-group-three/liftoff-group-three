ede3b7d3364335bc99ca4a8cd9c4299b65ffa79f
Christi-Lee <christileecurrier@gmail.com>
2024-03-15 06:39:02 -0500

    login error debug

---
+++ src/main/java/org/example/controllers/DashboardController.java
@@ -0,0 +1,13 @@
+package org.example.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+
+    @GetMapping("/dashboard")
+    public String showDashboard() {
+        return "goalsDashboard";
+    }
+}
